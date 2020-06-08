75a2ef34b4468cfca7d8d0645c65ea2d88621a10
ashleybiermann <ashleybiermann@gmail.com>
2020-06-08 15:29:10 -0700

    for submission

--- code-challenges/array-reverse/array-reverse.test.js
+++ code-challenges/array-reverse/array-reverse.test.js
@@ -5,10 +5,10 @@ const arrayReverse = require('./array-reverse.js');
 describe('Testing array code challenges', () => {
   it('Should reverse an array', () => {
     //here is where js goes
-    const testArray = [1,2,3,4,5];
+    const testArray = [1,2,3,4,5,6];
     const reverseArray = arrayReverse(testArray);
 
     expect(reverseArray).not.toStrictEqual(testArray);
-    expect(reverseArray).toStrictEqual([5,4,3,2,1]);
+    expect(reverseArray).toStrictEqual([6,5,4,3,2,1]);
   });
 });
