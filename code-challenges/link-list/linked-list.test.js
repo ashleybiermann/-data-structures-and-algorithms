a4705cdea47eebb742d6eea954a6d3ed56cfdec7
ashleybiermann <ashleybiermann@gmail.com>
2020-06-15 09:48:01 -0700

    outlined testing file for linked-list

---
+++ code-challenges/link-list/linked-list.test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const linkedList = require('./linked-list.js');
+
+// 1 can instantiate an empty list
+// 2 can insert into list
+// 3 head points to first node
+// 4 can insert multiple nodes
+// 5 will return true when finding a value that exists in the list
+// 6 will return false when searching for a value that doesn't exist
+// can return a collection of all values that exist
+
+describe('')
