c3be41df88c2a005aea2d09be77e591cf8c042b4
ashleybiermann <ashleybiermann@gmail.com>
2020-06-15 22:16:06 -0700

    complete 06 code challenges and one passing test

--- code-challenges/link-list/linked-list.test.js
+++ code-challenges/link-list/linked-list.test.js
@@ -1,8 +1,7 @@
 'use strict';
 
-const linkedList = require('./linked-list.js');
+const LinkedList = require('./linked-list.js');
 
-// 1 can instantiate an empty list
 // 2 can insert into list
 // 3 head points to first node
 // 4 can insert multiple nodes
@@ -10,4 +9,17 @@ const linkedList = require('./linked-list.js');
 // 6 will return false when searching for a value that doesn't exist
 // can return a collection of all values that exist
 
-describe('')
+// 1 Can successfully add a node to the end of the linked list
+// 2 Can successfully add multiple nodes to the end of a linked list
+// 3 Can successfully insert a node before a node located i the middle of a linked list
+// Can successfully insert a node before the first node of a linked list
+// 4 Can successfully insert after a node in the middle of the linked list
+// 5 Can successfully insert a node after the last node of the linked list
+
+describe('Testing linked-list challenges', () => {
+  it('Should create an empty linked-list', () => {
+    const testList = new LinkedList();
+    console.log(testList);
+    expect(testList).toEqual( {head: null} );
+  });
+});
