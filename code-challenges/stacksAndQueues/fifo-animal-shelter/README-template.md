c1199a3f4f42823fcd4b9ee464955ba0058f35f3
ashleybiermann <ashleybiermann@gmail.com>
2020-06-23 16:19:44 -0700

    challenge completed, now to testing...

---
+++ code-challenges/stacksAndQueues/fifo-animal-shelter/README-template.md
@@ -0,0 +1,11 @@
+# FIFO - Animal Shelter
+
+### Create a class called AnimalShelter which holds only dogs and cats, it uses FIFO approach. Implement the following methods:
+- `enqueue(animal)` - add `animal` to the shelter, `animal` can be either dog or cat
+- `dequeue(pref)` - returns either a dog or cat. If `pref` is not `'dog'` or `'cat'`, return null
+
+### Approach & Efficiency
+- O(1)? etc
+
+### Solution
+![Whiteboard for...]()
---
+++ code-challenges/stacksAndQueues/fifo-animal-shelter/fifo-animal-shelter.js
@@ -0,0 +1,54 @@
+'use strict';
+
+class Animal {
+  constructor(name, type) {
+    this.name = name;
+    this.type = type;
+  }
+}
+
+class AnimalShelter {
+  constructor() {
+    this.dogArr = [];
+    this.catArr = [];
+  }
+
+  enqueue(animal){
+    if (animal.type === 'dog') {
+      this.dogArr.push(animal);
+    } else {
+      this.catArr.push(animal);
+    }
+  }
+  
+  dequeue(pref) {
+    let luckyAnimal = '';
+    if (pref === 'dog') {
+      luckyAnimal = this.dogArr[0];
+      this.dogArr.shift();
+    } else if (pref === 'cat') {
+      luckyAnimal = this.catArr[0];
+      this.catArr.shift();
+    } else {
+      return null;
+    }
+    return luckyAnimal;
+  }
+}
+
+let myShelter = new AnimalShelter;
+
+const doggy1 = new Animal('doggy1', 'dog');
+const doggy2 = new Animal('doggy2', 'dog');
+const kitty1 = new Animal('kitty1', 'cat');
+const kitty2 = new Animal('kitty2', 'cat');
+
+myShelter.enqueue(doggy1);
+myShelter.enqueue(doggy2);
+myShelter.enqueue(kitty1);
+myShelter.enqueue(kitty2);
+
+console.log(myShelter.dequeue('cat'));
+console.log(myShelter);
+
+module.exports = AnimalShelter;
