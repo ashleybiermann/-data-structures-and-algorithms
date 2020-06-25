fc73e7f66801e9cfc1e0f75eb83d7cc4f82105d6
ashleybiermann <ashleybiermann@gmail.com>
2020-06-24 18:18:35 -0700

    tests written and passing

--- code-challenges/multiBracketValidation/multi-bracket-validation.test.js
+++ code-challenges/multiBracketValidation/multi-bracket-validation.test.js
@@ -2,3 +2,18 @@
 
 const multiBracketValidation = require('./multi-bracket-validation');
 
+describe('Testing the multiBracketValidation function', () => {
+  it('Should return true', () => {
+    expect(multiBracketValidation('()[[Extra Characters]]')).toBe(true);
+  });
+  it('Should return true', () => {
+    expect(multiBracketValidation('{}{Code}[Fellows](())')).toBe(true);
+  });
+  it('Should return false', () => {
+    expect(multiBracketValidation('[({}]')).toBe(false);
+  });
+  it('Should return false', () => {
+    expect(multiBracketValidation('{(})')).toBe(false);
+  });
+})
+
